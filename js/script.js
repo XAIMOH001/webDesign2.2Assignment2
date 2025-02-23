f73f722dd30d47e085ee6e23d8b79141e834ede7
xaimohchau@gmail.com <xaimohchau@gmail.com>
2025-02-23 18:12:36 +0300

    modified the nav bar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+document.addEventListener("click", function (event) {
+    let nav = document.querySelector("nav");
+    let menuIcon = document.querySelector(".menu-icon");
+
+    // Check if the click is outside the navigation and menu icon
+    if (!nav.contains(event.target) && !menuIcon.contains(event.target)) {
+        nav.classList.remove("active"); // Hide the menu
+    }
+});
+
 function toggleMenu() {
     let nav = document.querySelector("nav");
     nav.classList.toggle("active");
